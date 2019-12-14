5b9b68837741020b6f11d5bbf40c5e964dbb48a1
Noah Baltensperger <nbalten@gmail.com>
2019-12-14 13:34:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+const submitBtn = document.getElementById('reg-btn');
+const username = document.getElementById('username');
+const usernameError = document.getElementById('username-error');
+const email = document.getElementById('email');
+const emailError = document.getElementById('email-error')
+const password = document.getElementById('password');
+const passwordError = document.getElementById('password-error');
+const confirmPassword = document.getElementById('confirm-password');
+const confirmPasswordError = document.getElementById('confirm-password-error');
+const signupBtn = document.getElementById('sign-up-btn');
+const loginBtn = document.getElementById('sign-in-btn');
+const contain = document.getElementsByClassName('container');
+const emailDiv = document.getElementById('email-div');
+const confirmPasswordDiv = document.getElementById('confirm-password-div');
+const myForm = document.getElementById('form');
+
+function lightUp() {
+	signupBtn.classList.add('form-btnactive')
+
+}
+
+
+loginBtn.addEventListener('click', function() {
+	emailDiv.classList.add('signin')
+	confirmPasswordDiv.classList.add('signin')
+	loginBtn.classList.add('form-btnactive')
+	signupBtn.classList.remove('form-btnactive')
+
+
+});
+
+signupBtn.addEventListener('click', function() {
+	emailDiv.classList.remove('signin')
+	confirmPasswordDiv.classList.remove('signin')
+	signupBtn.classList.add('form-btnactive')
+	loginBtn.classList.remove('form-btnactive')
+})
+
+
+function errorHandle() {
+
+	usernameError.textContent = '';
+	passwordError.textContent = '';
+	emailError.textContent = ''
+	confirmPasswordError.textContent = '';
+
+	if(username.value === '') {
+		usernameError.textContent = "Please enter Username"
+		return false;
+	}
+
+	else if( /(.+)@(.+){2,}\.(.+){2,}/.test(email.value) == false) {
+		emailError.textContent = "Email is Invalid"	
+		return false;
+	} 
+		 
+
+	else if(password.value === '') {
+		passwordError.textContent = "Please enter Password"
+		return false;
+	}
+
+	else if(confirmPassword.value != password.value) {
+		confirmPasswordError.textContent = "Your password does not match"
+		return false;
+	}
+	return true;
+	
+
+	/*
+	const request = newXMLHttpRequest();
+
+	request.open("post", "form.php");
+	request.onload = function() {
+		console.log(request.responseText);
+	} 
+	request.send(new FormData(myForm));
+
+	*/
+}
+
+
+
+
+
+
+
+
+
